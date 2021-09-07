a83adedacc67bed88e5ecc59ef4e2dc7d2ec3066
Adam Niziołek <niziolek.adam@gmail.com>
2021-09-07 02:50:18 +0200

    Created contact form logic and created and styled form alerts

--- src/components/HomeWhoWeHelpList.js
+++ src/components/HomeWhoWeHelpList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export default function HomeWhoWeHelpList({data, selected, currentPage, setCurrentPage}) {
     const linesPerPage = 3;

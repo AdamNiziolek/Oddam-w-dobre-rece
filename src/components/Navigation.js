d3d0c480857ba8b38385352d68bc12795ff0e1bc
Adam Niziołek <niziolek.adam@gmail.com>
2022-04-19 14:54:26 +0200

    Deleted navigation element

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,6 @@ import { Link } from "react-scroll"
 export default function Navigation() {
     return (
         <div className="header__nav">
-            <Link className="header__nav__link" to="main-section" smooth={true} duration={500}>Start</Link>
             <Link className="header__nav__link" to="easy-steps" smooth={true} duration={500}>O co chodzi?</Link>
             <Link className="header__nav__link" to="about-us" smooth={true} duration={500}>O nas</Link>
             <Link className="header__nav__link" to="who-we-help" smooth={true} duration={500}>Fundacja i organizacje</Link>

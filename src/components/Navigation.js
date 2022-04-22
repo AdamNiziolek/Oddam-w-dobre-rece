86397a7df83cd5c11de5e04f382ced21cef2b61c
Adam Niziołek <niziolek.adam@gmail.com>
2022-04-22 09:35:39 +0200

    Resolved navigation problem

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import { Link } from "react-scroll"
+import { useHistory } from "react-router-dom"
 
 export default function Navigation() {
+    const history = useHistory();
+    const handleChange = () => history.push("/");
+
     return (
-        <div className="header__nav">
-            <Link className="header__nav__link" to="easy-steps" smooth={true} duration={500}>O co chodzi?</Link>
-            <Link className="header__nav__link" to="about-us" smooth={true} duration={500}>O nas</Link>
-            <Link className="header__nav__link" to="who-we-help" smooth={true} duration={500}>Fundacja i organizacje</Link>
-            <Link className="header__nav__link" to="contact" smooth={true} duration={500}>Kontakt</Link>
+        <div className="header__nav" >
+            <Link 
+                className="header__nav__link" 
+                to="main-section" smooth={true} duration={500} 
+                onClick={handleChange}>
+                Start
+            </Link>
+            <Link 
+                className="header__nav__link" 
+                to="easy-steps" smooth={true} duration={500} 
+                onClick={handleChange}>
+                O co chodzi?
+            </Link>
+            <Link 
+                className="header__nav__link"
+                to="about-us" smooth={true} duration={500} 
+                onClick={handleChange}>
+                O nas
+            </Link>
+            <Link 
+                className="header__nav__link"
+                to="who-we-help" smooth={true} duration={500} 
+                onClick={handleChange}>
+                Fundacja i organizacje
+            </Link>
+            <Link 
+                className="header__nav__link" 
+                to="contact" smooth={true} duration={500}
+                onClick={handleChange}>
+                Kontakt
+            </Link>
         </div>
     )
 }

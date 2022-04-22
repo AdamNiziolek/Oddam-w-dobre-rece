d47465e0c08319527dd00526b7eadcb17b60dc8d
Adam Niziołek <niziolek.adam@gmail.com>
2022-04-22 18:42:57 +0200

    Added classname functionality

--- src/components/Home/WhoWeHelp/WhoWeHelp.js
+++ src/components/Home/WhoWeHelp/WhoWeHelp.js
@@ -1,28 +1,22 @@
 import React, { useState } from 'react';
+import classNames from 'classnames'
 import WhoWeHelpList from './WhoWeHelpList';
 import whoWeHelpList from '../../../config/whoWeHelpList.json';
 
 export default function WhoWeHelp() {
     const [selected, setSelected] = useState('fundations');
-    const [classF, setClassF] = useState("who-we-help__buttons__element border");
-    const [classO, setClassO] = useState("who-we-help__buttons__element");
-    const [classL, setClassL] = useState("who-we-help__buttons__element");    
     const [currentPage, setCurrentPage] = useState(1);
     const list = whoWeHelpList;
 
-    if (selected === "fundations" && classF !== "who-we-help__buttons__element border") {
-        setClassF("who-we-help__buttons__element border");
-        setClassO("who-we-help__buttons__element");
-        setClassL("who-we-help__buttons__element");
-    } else if (selected === "organizations" && classO !== "who-we-help__buttons__element border") {
-        setClassF("who-we-help__buttons__element");
-        setClassO("who-we-help__buttons__element border");
-        setClassL("who-we-help__buttons__element"); 
-    } else if (selected === "localCollections" && classL !== "who-we-help__buttons__element border") {
-        setClassF("who-we-help__buttons__element");
-        setClassO("who-we-help__buttons__element");
-        setClassL("who-we-help__buttons__element border");
+    function getClassName(selectedItem) {
+        return classNames({
+            "who-we-help__buttons__element": true,
+            "border": selected === selectedItem
+        });
     }
+    const classF = getClassName('fundations');
+    const classO = getClassName('organizations');
+    const classL = getClassName('localCollections');
 
     return (
         <div className="who-we-help" id="who-we-help">
@@ -31,24 +25,24 @@ export default function WhoWeHelp() {
             <div className="who-we-help__buttons">
                 <div className={classF}
                     id="fundations"
-                    onClick={(e) => {
-                        setSelected(e.target.id);
+                    onClick={(event) => {
+                        setSelected(event.target.id);
                         setCurrentPage(1);
                     }}>
                     Fundacjom
                 </div>
                 <div className={classO}
                     id="organizations"
-                    onClick={(e) => {
-                        setSelected(e.target.id);
+                    onClick={(event) => {
+                        setSelected(event.target.id);
                         setCurrentPage(1);
                     }}>
                     Organizacjom pozarządowym
                 </div>
                 <div className={classL}
                     id="localCollections"
-                    onClick={(e) => {
-                        setSelected(e.target.id);
+                    onClick={(event) => {
+                        setSelected(event.target.id);
                         setCurrentPage(1);
                     }}>
                     Lokalnym zbiórkom

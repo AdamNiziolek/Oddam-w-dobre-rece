5415d0501f093d2c76c2fb19e651372ddb20a749
Adam Niziołek <niziolek.adam@gmail.com>
2022-04-22 18:30:38 +0200

    Add list to config file

--- src/components/Home/WhoWeHelp/WhoWeHelp.js
+++ src/components/Home/WhoWeHelp/WhoWeHelp.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import WhoWeHelpList from './WhoWeHelpList';
+import whoWeHelpList from '../../../config/whoWeHelpList.json';
 
 export default function WhoWeHelp() {
     const [selected, setSelected] = useState('fundations');
@@ -7,106 +8,7 @@ export default function WhoWeHelp() {
     const [classO, setClassO] = useState("who-we-help__buttons__element");
     const [classL, setClassL] = useState("who-we-help__buttons__element");    
     const [currentPage, setCurrentPage] = useState(1);
-    const list = {
-        fundations: [
-            {
-                title: 'Fundacja "Dbam o Zdrowie"',
-                subtitle: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
-                staff: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-            },
-            {
-                title: 'Fundacja "Dla dzieci"',
-                subtitle: 'Cel i misja: Pomoc dzieciom z ubogich rodzin.',
-                staff: 'ubrania, meble, zabawki'
-            },
-            {
-                title: 'Fundacja "Bez domu"',
-                subtitle: 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.',
-                staff: 'ubrania, jedzenie, ciepłe koce'
-            },
-            {
-                title: 'Fundacja 4',
-                subtitle: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
-                staff: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-            },
-            {
-                title: 'Fundacja 5',
-                subtitle: 'Cel i misja: Pomoc dzieciom z ubogich rodzin.',
-                staff: 'ubrania, meble, zabawki'
-            },
-            {
-                title: 'Fundacja 6',
-                subtitle: 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.',
-                staff: 'ubrania, jedzenie, ciepłe koce'
-            },
-            {
-                title: 'Fundacja 7',
-                subtitle: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
-                staff: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-            },
-            {
-                title: 'Fundacja 8',
-                subtitle: 'Cel i misja: Pomoc dzieciom z ubogich rodzin.',
-                staff: 'ubrania, meble, zabawki'
-            },
-            {
-                title: 'Fundacja 9',
-                subtitle: 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.',
-                staff: 'ubrania, jedzenie, ciepłe koce'
-            }
-        ],
-        organizations: 
-        [
-            {
-                title: 'Organizacja "Dbam o Zdrowie"',
-                subtitle: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
-                staff: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-            },
-            {
-                title: 'Organizacja "Dla dzieci"',
-                subtitle: 'Cel i misja: Pomoc dzieciom z ubogich rodzin.',
-                staff: 'ubrania, meble, zabawki'
-            },
-            {
-                title: 'Organizacja "Bez domu"',
-                subtitle: 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.',
-                staff: 'ubrania, jedzenie, ciepłe koce'
-            },
-            {
-                title: 'Organizacja 4',
-                subtitle: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
-                staff: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-            },
-            {
-                title: 'Organizacja 5',
-                subtitle: 'Cel i misja: Pomoc dzieciom z ubogich rodzin.',
-                staff: 'ubrania, meble, zabawki'
-            },
-            {
-                title: 'Organizacja 6',
-                subtitle: 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.',
-                staff: 'ubrania, jedzenie, ciepłe koce'
-            }
-        ],
-        localCollections: 
-        [
-            {
-                title: 'Lokalna zbiórka "Dbam o Zdrowie"',
-                subtitle: 'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej.',
-                staff: 'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-            },
-            {
-                title: 'Lokalna zbiórka "Dla dzieci"',
-                subtitle: 'Cel i misja: Pomoc dzieciom z ubogich rodzin.',
-                staff: 'ubrania, meble, zabawki'
-            },
-            {
-                title: 'Lokalna zbiórka "Bez domu"',
-                subtitle: 'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.',
-                staff: 'ubrania, jedzenie, ciepłe koce'
-            }
-        ]
-    };
+    const list = whoWeHelpList;
 
     if (selected === "fundations" && classF !== "who-we-help__buttons__element border") {
         setClassF("who-we-help__buttons__element border");
